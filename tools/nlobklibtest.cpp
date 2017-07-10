cb9e9e206da53faa62ced2ea33948f83d6c4ebf7
Heikki Mäntysaari <mantysaari@bnl.gov>
2017-07-10 11:10:06 -0400

    Cleanup nlobklibtester

--- tools/nlobklibtest.cpp
+++ tools/nlobklibtest.cpp
@@ -31,18 +31,12 @@ int main()
     double qs0sqr = 1.0;    // Q_s,0^2 at x=0.01 (GeV^2)
     double e_c = 1.0;
     double anomalous_dimension = 1.0;  // probably want to keep constant
+    
+    // BK solver
     double alphas_scaling = 1.0;     // C^2 in the expression for alpha_s
     
     
-    
-    MV ic;
-    ic.SetQsqr(qs0sqr);
-    ic.SetAnomalousDimension(anomalous_dimension);
-    ic.SetE(e_c);       // e_c of MVe parametrization
-    ic.SetLambdaQcd(0.241);
-    Dipole dipole(&ic);
-    
-    // Set other configurations
+    // ***Set other configurations***
     double maxy = 1.2;      // Solve BK up to this rapidity
     config::RC_LO = config::BALITSKY_LO; // Balitsky running coupling for LO kernel
     config::RESUM_RC = config::RESUM_RC_PARENT; // Parent dipole in the resummation
@@ -52,10 +46,21 @@ int main()
     config::KSUB = 0.6;  // Optimal value for K_sub
     config::NO_K2 = true;  // Do not include numerically demanding full NLO part
     
-    // Solve up to some rapidity
+    
+    // ***Solve resummed BK***
+    MV ic;   // Initial condition
+    ic.SetQsqr(qs0sqr);
+    ic.SetAnomalousDimension(anomalous_dimension);
+    ic.SetE(e_c);       // e_c of MVe parametrization
+    ic.SetLambdaQcd(0.241);
+    Dipole dipole(&ic);
+    
     BKSolver solver(&dipole);
     solver.SetAlphasScaling(alphas_scaling);
     solver.Solve(maxy);  // Solve up to maxy
+    
+    
+    // ***Study solution***
     
     // Give solution to the AmplitudeLib object
     AmplitudeLib DipoleAmplitude(solver.GetDipole()->GetData(), solver.GetDipole()->GetYvals(), solver.GetDipole()->GetRvals());
