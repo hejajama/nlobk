909952afff1afb1b99b4bf167594a13318e3bcee
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2024-08-01 14:11:00 +0300

    Remove unnecessary code

--- tools/config.hpp
+++ tools/config.hpp
@@ -34,99 +34,14 @@ namespace Amplitude
 
 
 
-    // Other constants
 
-    const double eps=0.000001;
 
     // Inline functions
 
     #define LINEINFO __FILE__ << ":" << __LINE__
 
     inline double SQR(const double x) { return x*x; }
-
-    enum Parton
-    {
-        UVAL,   ///< valence u
-        DVAL,   ///< valence d
-        USEA,   ///< sea u
-        DSEA,   ///< sea d
-        U,      ///< u (valence + sea)
-        D,      ///< d (valence + sea)
-        S,      ///< strange
-        C,      ///< charm
-        B,      ///< bottom
-        G,      ///< gluon
-        UBAR,   ///< anti-u
-        DBAR,   ///< anti-d
-        SBAR,   ///< anti-s
-		CBAR,   ///< anti-c
-		BBAR,   ///< anti-b
-        LIGHT	///< light quarks = u+d+s
-    };
-
-    /**
-     * Selection between NLO and LO distributions
-     */
-    enum Order
-    {
-        LO,
-        NLO
-    };
-
-    enum RunningAlphas
-    {
-        RUNNING,
-        FIXED
-    };
-
-    /**
-     * Representation at which the dipole amplitude is evaluated
-     */
-    enum Representation
-    {
-        FUNDAMENTAL,
-        ADJOINT
-    };
-
-    enum Hadron
-    {
-        PI,   ///< pi+, pi- (charged pion)
-        PIP,  ///< pi+
-        PIM,  ///< pi-
-        PI0,  ///< pi0 (neutral pion)
-        K,    ///< K+, K- (charged kaon)
-        KP,   ///< K+
-        KM,   ///< K-
-        K0,   ///< K0, bar K0 (neutral kaon)
-        P,    ///< p, bar p (proton, antiproton)
-        PP,   ///< p+
-        PM,   ///< p-
-        NE,   ///< neutron, antineutron
-        H,    ///< sum of charged hadrons
-        HP,   ///< postiive hadron
-        HM    ///< negative hadron
-    };
-
-    /**
-     * Fourier transfer method used
-     */
-    enum FT_Method
-    {
-            GSL,            ///< Direct calculation using GSL integration method
-            ACC_SERIES      ///< Series method implemented in fourier/fourier.c
-    };
-
-    /**
-     * Virtual photon polarizatoin states
-     */
-    enum Polarization
-    {
-        L,  ///<< Longitudinal polarization
-        T   ///<< Transverse polarization
-    };
-
 }
-
 
 
 #endif
--- tools/tools.hpp
+++ tools/tools.hpp
@@ -23,21 +23,9 @@ double Alpha_s(double Qsqr, double scaling=1.0);
 double Alpha_s_r(double rsqr, double scaling=1.0);
 double Alphabar_s(double Qsqr, double scaling=1.0); // \alpha_s N_C / Pi
 */
-double T_A(double b, int A);	// Transverse density profile of the nucleus normalized by A
-								// Unit: [b]=1/GeV!
-void InitializeWSDistribution(int A);
-
 int FindIndex(double val, std::vector<double> &array);
 
-// Subtract the samallest element of the array from each element
-void SubtractMinimum(std::vector<double> &array);	
-
 std::string Alpha_s_str();
-
-/**
- * Convert parton type to std::string
- */
-std::string PartonToString(Amplitude::Parton p);
 
 // Running coupling
 const double MAXALPHA = 0.7;
